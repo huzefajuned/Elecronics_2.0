e01315fe120321dd3650841e604490d9225b0909
Huzefa Bin juned <huzefabinjuned@Huzefas-MacBook-Pro.local>
2022-06-18 14:12:18 +0530

    admin pannel completed

--- Connection/DataBase.js
+++ Connection/DataBase.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config()
+const DB_URL = process.env.DB_URL
 
 const DataBase =
-    mongoose.connect(process.env.DataBase).then(() => {
+    mongoose.connect(DB_URL).then(() => {
         console.log(" Connected To Electronics DataBase")
     })
         .catch(
