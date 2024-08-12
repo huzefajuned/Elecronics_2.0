28afc7f4292095d99c4d288ed901177974423b40
huzefa <huzefa@huzefas-MacBook-Air.local>
2024-08-12 20:22:09 +0530

    updated : project structure updated

--- Routes/auth.buyer.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const authBuyerController = require("../Controllers/auth.buyer.controller");
-
-router.post("/buyerRegister", authBuyerController.buyerRegister);
-router.post("/buyerLogin", authBuyerController.buyerLogin);
-
-module.exports = router;
