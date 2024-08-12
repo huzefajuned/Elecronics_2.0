ac7c37d2b06c7f853bb44f48a8205124a0f85195
huzefa <huzefa@huzefas-MacBook-Air.local>
2024-08-12 20:13:55 +0530

    update : vite added

---
+++ Client/src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "react-toastify/dist/ReactToastify.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta aliquam,
+      repellat corrupti dolorem ratione a eos pariatur molestias corporis sunt
+      blanditiis temporibus iusto. At, ab soluta delectus ipsum fuga autem?
+    </p>
+  </React.StrictMode>
+);
